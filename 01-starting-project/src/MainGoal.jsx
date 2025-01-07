004071927fff70e4b91ddcfaa089cde16764c299
Zakyboss <Boochimo8@gmail.com>
2025-01-07 22:52:21 +0300

    Custom components intro

---
+++ 01-starting-project/src/MainGoal.jsx
@@ -0,0 +1,13 @@
+import ReactLogo from './assets/react-core-concepts.png';
+
+export default function MainGoal (){
+    return (
+        < >
+                    <img src={ReactLogo} alt='reactLogo' className='reactLogo' style={{marginLeft:'44%'}}/>
+                    <div className="goalContainer">
+ <h2>My Main Goal of learning react is To become a confident Fullstack <b style={{color:'greenyellow'}}>Mern</b>  developer baby!</h2>
+        </div>
+        </>
+        
+    )
+}
