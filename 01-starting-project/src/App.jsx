c00d82f9523aab6b3c5f6c004ffcd22135077aab
Zakyboss <Boochimo8@gmail.com>
2025-01-09 17:25:49 +0300

    Using Prop.children

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,15 @@
 import Content from './CoreConcept'
 import { CORE_CONCEPTS } from './data';
+import TabButton from './TabButton';
 function App() {
   return (
     <div>
             <h3 style={{marginLeft:'40%'}}>Core Concepts</h3>
 
-      <main id = 'core-concepts' style={{display:'flex'}}>
+      <main >
+        <section id = 'core-concepts' style={{display:'flex'}}>
+
+       
         <ul>
   {
     CORE_CONCEPTS.map(concept=> {
@@ -13,7 +17,20 @@ function App() {
     })
   }
 </ul>
+</section>
+
+<section id='examples'>
+        <h1>Examples</h1>
+        <menu>
+        <TabButton>Components</TabButton>
+        <TabButton>JSX</TabButton>
+        <TabButton>Props</TabButton>
+        <TabButton>State</TabButton>
+
+        </menu>
+      </section>
       </main>
+  
     </div>
   );
 }
