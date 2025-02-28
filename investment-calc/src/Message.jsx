ef93e6a37a3dec7c25e5f8965158abd988beab75
Zakyboss <Boochimo8@gmail.com>
2025-02-28 14:42:40 +0300

    Created message component

---
+++ investment-calc/src/Message.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './Message.module.css'
+export default function Message() {
+  return (
+    <div className={styles.msg}>Start Investing Today 🤑</div>
+  )
+}
