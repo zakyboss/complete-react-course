50def13a4688093b0c9645faefd5f8f84461b1ac
Zakyboss <Boochimo8@gmail.com>
2025-03-19 16:02:16 +0300

    Adding timer component and passing props

--- 03-Refs-Portals/src/App.jsx
+++ 03-Refs-Portals/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import Player from "./components/Player";
+import TimerChallenge from "./TimerChallenge";
 function App() {
-  // const [myName , setMyName] = useState("")
   return (
     <>
       <Player/>
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title='Easy' targetTime={1}/>
+        <TimerChallenge title='Not easy' targetTime={5}/>
+        <TimerChallenge title='Getting tough' targetTime={10}/>
+        <TimerChallenge title='Pros only' targetTime={15}/>
+      </div>
     </>
   );
 }
