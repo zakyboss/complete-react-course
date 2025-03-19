99eb9d1c72de38915ae3149c326e897ffacf2448
Zakyboss <Boochimo8@gmail.com>
2025-03-19 16:01:40 +0300

    Creating individual challenges

---
+++ 03-Refs-Portals/src/TimerChallenge.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function TimerChallenge({title,targetTime}) {
+  return (
+    <section className='challenge'>
+        <h2>{title}</h2>
+        <p className='challenge-time'>
+        {targetTime}second{targetTime>1?'s':''}
+       <p>
+       <button> 
+
+       </button>
+       </p>
+        </p>
+
+<p className=''>  Time is running .../ Timer inactive</p>
+
+         </section>
+  )
+}
