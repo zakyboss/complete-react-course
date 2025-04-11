c7f8fe23592c6fc8b7364399766f8da13bf28b25
Zakyboss <Boochimo8@gmail.com>
2025-04-11 18:39:59 +0300

    Sidebar content

---
+++ task-manager/01-starting-project/src/Components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function Sidebar() {
+  return (
+    <aside className='w-1/3 px-8 py-16 bg-stone-900  text-stone-50 md:w-72  rounded-r-xl'>
+  <h3>Your projects</h3>
+  <button>
+    +Add Project
+  </button>
+  <ul>
+
+  </ul>
+    </aside>
+  )
+}
