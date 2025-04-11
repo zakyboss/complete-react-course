842d1e431936da15947d53228636144df524f756
Zakyboss <Boochimo8@gmail.com>
2025-04-11 18:38:45 +0300

    Creating and styling input component

---
+++ task-manager/01-starting-project/src/Components/Input.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ({ label, textarea, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600  focus:outline-none focus:border-stone-600";
+  return (
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea {...props} className={classes} />
+      ) : (
+        <input {...props} className={classes} />
+      )}
+    </p>
+  );
+}
